eba4155cbf6abfbfe9e4e5ba8ccb45e012e87cd2
chenwei <13813641925@163.com>
2020-07-17 11:04:59 +0800

    Initial commit 1.springboot ，provider ，consumer搭建

---
+++ src/main/java/com/cw/dubbo/film_dubbo/FilmDubboApplication.java
@@ -0,0 +1,13 @@
+package com.cw.dubbo.film_dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmDubboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cw/dubbo/film_dubbo/FilmDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw.dubbo.film_dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
